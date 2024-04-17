17b7deb105e0f5004ff4f877f79f0ac953a27583
Jones Cian <jonescian@Joness-Laptop.local>
2024-04-17 14:50:42 +0200

    🔨 Add or update developement scripts.

---
+++ main.py
@@ -0,0 +1,28 @@
+# * Import the necessary libraries
+import os
+from dotenv import load_dotenv
+
+import discord
+
+# * Load the environment variables
+load_dotenv()
+DISCORD_TOKEN = os.getenv('TOKEN')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(DISCORD_TOKEN) # type: ignore
