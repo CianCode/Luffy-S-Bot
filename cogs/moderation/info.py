9d45d665f174aca791693f93d191e1d6a60fe260
Jones Cian <jones.cian@outlook.fr>
2024-04-17 17:55:36 +0200

    🚀 Deploy Stuff

--- cogs/moderation/info.py
+++ cogs/moderation/info.py
@@ -2,13 +2,13 @@
 import discord
 from discord.ext import commands
 
-class ReputationRoles(commands.Cog):
+class InfoBot(commands.Cog):
 
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    # TODO: Add the reputation roles command to add role to the database
+    # TODO: Add the info command to get the bot information (Slash Commands or Hybrid Commands)
 
 
 async def setup(bot):
-    await bot.add_cog(ReputationRoles(bot))
+    await bot.add_cog(InfoBot(bot))
--- main.py
+++ main.py
@@ -2,27 +2,56 @@
 import os
 from dotenv import load_dotenv
 
+# * Import the necessary discord libraries
 import discord
+from discord.ext import commands
 
 # * Load the environment variables
 load_dotenv()
 DISCORD_TOKEN = os.getenv('TOKEN')
+DATABASE_URL = os.getenv('DATABASE_URL')
+PREFIX = os.getenv('PREFIX')
 
-intents = discord.Intents.default()
-intents.message_content = True
+# TODO: Add the database connection using pymongo
 
-client = discord.Client(intents=intents)
+# * Create the cog file manager
+class Cogs(commands.Cog):
+    def __init__(self, bot: commands.Bot) -> None:
+        self.bot = bot
 
-@client.event
-async def on_ready():
-    print(f'We have logged in as {client.user}')
+    @commands.Cog.listener(name='on_ready')
+    async def loader(self) -> None:
+        for root, _, files in os.walk('./cogs'):
+            for filename in files:
+                if filename.endswith('.py'):
+                    try:
+                        path = os.path.join(root, filename)[len("./cogs/"):][:-3].replace(os.path.sep, '.')
+                        await self.bot.load_extension(f'cogs.{path}')
+                        print(f'🟢 SUCCES | Loaded the cog: {filename}')
+                    except Exception as e:
+                        print(f'❌ ERROR | {e}')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+# * Creation of the bot class
+class Luffy(commands.Bot):
+    def __init__(self) -> None:
+        super().__init__(command_prefix=PREFIX, intents=discord.Intents.all()) # type: ignore
 
-client.run(DISCORD_TOKEN) # type: ignore
+    async def setup_hook(self) -> None:
+        await self.add_cog(Cogs(self))
+
+        await self.tree.sync()
+
+    async def on_ready(self) -> None:
+        await self.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.watching, name=f"Hello world!"))
+
+        print(f'Logged in as {self.user}')
+        print(f'--------------------------')
+
+    async def on_connect(self) -> None:
+        await self.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.watching, name=f"Initialisation..."))
+
+luffy = Luffy()
+
+if __name__ == '__main__':
+    luffy.run(DISCORD_TOKEN) # type: ignore
